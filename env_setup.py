5eb788eb07a8c5497e489ea5a8b91506f7db3d60
James Merrill <james.merrill@mail.mcgill.ca>
2024-07-26 12:27:00 -0400

    Update env_setup.py

--- env_setup.py
+++ env_setup.py
@@ -1,6 +1,2 @@
-conda create -n pm3env -c conda-forge "python=3.8" libpython mkl-service m2w64-toolchain numba python-graphviz scipy
-conda activate pm3env
-conda install pymc3
-conda install spyder=4.2.5
-conda remove theano
-conda install -c conda-forge theano-pymc
+conda create -c conda-forge -n pymc_env "pymc>=5"
+conda activate pymc_env
